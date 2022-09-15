7011753c9bae97e6e017d3aafac921215392c574
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-09-16 00:47:28 +0700

    add method to decrypt aes/cipher GCM

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	mode := flag.String("mode", "encrypt", "Put mode here, available mode (encrypt/decrypt)")
+	flag.Parse()
 
 	fmt.Printf("Input Text: ")
 	scanner.Scan()
@@ -34,7 +35,14 @@ func main() {
 		fmt.Println("Encrypted:", string(encText))
 
 	case "decrypt":
+		decText, err := DecryptAES256GCM([]byte(inputText), []byte(key))
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Decrypted:", string(decText))
 	default:
+		panic("error: invalid flags!")
 	}
 }
 
@@ -59,4 +67,35 @@ func EncryptAES256GCM(text, key []byte) ([]byte, error) {
 	s = base64.StdEncoding.EncodeToString([]byte(s))
 
 	return []byte(s), nil
+}
+
+func DecryptAES256GCM(text, key []byte) ([]byte, error) {
+	text, err := base64.StdEncoding.DecodeString(string(text))
+	if err != nil {
+		return nil, err
+	}
+
+	text, err = hex.DecodeString(string(text))
+	if err != nil {
+		return nil, err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonceSize := gcm.NonceSize()
+	nonce, encText := text[:nonceSize], text[nonceSize:]
+	res, err := gcm.Open(nil, nonce, encText, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
